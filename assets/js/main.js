a2ce91e76209ca1586e101f6d8b190a269c1d890
Mohammed Ismail Vajeehullah <vajeehvjk@gmail.com>
2025-07-09 02:58:40 +0530

    Commit 03: JS Code - Get Players Names from Register form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,40 @@
+// Start Greet Window
 window.addEventListener("DOMContentLoaded", () => {
     const greet = document.querySelector(".start-greet");
     const mainBody = document.querySelector(".main-body");
 
     // Wait for fade-out animation to complete (2s total)
     setTimeout(() => {
-        greet.style.display = "none";       // Remove greet section
-        mainBody.style.display = "block";   // Show main section
+        greet.style.display = "none";
+        mainBody.style.display = "block";
 
         setTimeout(() => {
             mainBody.style.opacity = "1";   // Fade it in
         }, 50);
-    }, 2000); // Match animation duration
+    }, 2000);
 });
 
+// Register Form Submited Actions & Set Input Players into Score Board
 const registerForm = document.querySelector(".register-form");
 const gameArea = document.querySelector(".game-area");
 
+const player1Input = document.querySelector("#player1");
+const player2Input = document.querySelector("#player2");
+
+const player1Set = document.querySelector(".player1-score h3");
+const player2Set = document.querySelector(".player2-score h3");
+
 registerForm.addEventListener("submit", (e) => {
-  e.preventDefault(); // Prevent page reload
+    e.preventDefault();
 
-  // Hide registration form
-  document.querySelector(".players-register").style.display = "none";
+    player1Set.textContent = player1Input.value.trim();
+    player2Set.textContent = player2Input.value.trim();
 
-  // Show game area
-  gameArea.style.display = "block";
+    document.querySelector(".players-register").style.display = "none";
+
+    gameArea.style.display = "block";
+
 });
+
+
+
